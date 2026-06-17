99e3c68319bed9905889860981c49667ecf6d423
skaikruuo <skaikruuo@gmail.com>
2026-06-17 16:45:00 +0300

    dz3

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -1,9 +1,10 @@
+from smartphone import Smartphone
 catalog = [
-    ("Samsung", "Galaxy S26 Ultra", "+79777421829")
-    ("POCO", "POCO X8", "+74957556983")
-    ("Apple", "iPhone 17", "+77846275821")
-    ("Honor", "Honor Magic 8 Pro 5G", "+79852696969")
-    ("Realme", "Realme GT 8 Pro", "+78696767564")
+    Smartphone("Samsung", "Galaxy S26 Ultra", "+79777421829"),
+    Smartphone("POCO", "POCO X8", "+74957556983"),
+    Smartphone("Apple", "iPhone 17", "+77846275821"),
+    Smartphone("Honor", "Honor Magic 8 Pro 5G", "+79852696969"),
+    Smartphone("Realme", "Realme GT 8 Pro", "+78696767564")
 ]
 for smartphone in catalog:
     print(f"{smartphone.brand}, {smartphone.model}, {smartphone.number}")
